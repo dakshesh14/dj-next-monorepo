569cab8bb00cde4e8cc43b1af01e9108b598bd47
dakshesh14 <dakshesh.jain14@gmail.com>
2023-10-04 20:35:17 +0530

    dev: first commit

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@ui/components/button";
+
+export default function Page() {
+  return (
+    <>
+      <h1 className="text-4xl font-bold">Web</h1>
+      <Button>Click me</Button>
+    </>
+  );
+}
